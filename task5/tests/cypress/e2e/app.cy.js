99b2dc7a53401b30d8078ab2c8f7ee3ccfa05c1c
Florian Tanner <fctanner@pm.me>
2024-04-17 19:04:17 +0200

    feat: all steps, add cy tests

--- task5/tests/cypress/e2e/app.cy.js
+++ task5/tests/cypress/e2e/app.cy.js
@@ -5,22 +5,24 @@ describe('app', () => {
 
   it('displays a title', () => {
     // Step 2:
-    // ?
+    cy.contains('Rhino App') // will look in entire page
   });
 
   it('greets user with their name', () => {
     // Step 3:
-    // ?
+    cy.get('#app-name')
+      .type('Rhino');
 
-    // cy.get('#app-greeting')
-    //   .contains('Hello Rhino!');
+    cy.get('#app-greeting') // get greeting output from app, prefix "app-"" can be used to refer by ID; "#" is selector
+     .contains('Hello Rhino!');
   });
 
   it('congratulates for checking all', () => {
     // Step 4:
-    // ?
+    cy.get('#app-choices input') // CSS selectors (inputs)
+      .check(); // will check ALL, important because number of boxes is random
 
-    // cy.get('#app-done')
-    //   .contains('Well done!');
+    cy.get('#app-done')
+      .contains('Well done!');
   });
 });
